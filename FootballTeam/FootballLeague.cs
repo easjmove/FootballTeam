505452091e4342a91091a6e6c7acd94926831f41
move <move@zealand.dk>
2025-09-30 11:41:45 +0200

    added validation rules

--- FootballTeam/FootballLeague.cs
+++ FootballTeam/FootballLeague.cs
@@ -1,10 +1,50 @@
-﻿namespace FootballTeamLib
+﻿using System.Xml.Linq;
+
+namespace FootballTeamLib
 {
     public class FootballTeam
 
     {
+        private string? _name;
+        private string? _league;
+
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string League { get; set; }
+        public string? Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(value);
+                }
+                if (value.Length < 2)
+                {
+                    throw new ArgumentOutOfRangeException(value);
+                }
+                _name = value;
+            }
+        }
+        public string League {
+            get
+            {
+                return _league;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(value);
+                }
+                if (value.Length < 2)
+                {
+                    throw new ArgumentOutOfRangeException(value);
+                }
+                _league = value;
+            }
+        }
     }
 }
